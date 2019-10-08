414a14b3e58eabdef55e76d960cfe38e18c9402d
lmagomes <lmagomes@gmail.com>
2019-10-08 21:22:02 +0100

    Some JS to auto adjust some positions.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,23 @@
 import '../css/styles';
+
+/*
+ * This is some js code to align #languages with #education. Not the most
+ * elegant way to do this, I'll admit.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    // check where #education starts
+    let rightStart = 0;
+    document.querySelectorAll('#showcase, #experience').forEach(el => {
+        rightStart += el.clientHeight;
+    });
+
+    // check where #languages starts
+    let leftStart = 0;
+    document.querySelectorAll('#showcase, #contacts, #skills').forEach(el => {
+        leftStart += el.clientHeight;
+    });
+
+    // set #languages top padding, so it starts at #education
+    const elLanguages = document.querySelector('#languages');
+    elLanguages.style.paddingTop = `${rightStart - leftStart}px`;
+});
